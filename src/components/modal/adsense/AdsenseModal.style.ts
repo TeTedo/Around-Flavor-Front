e149ef5fd535c2a33101749e6ad9c06050c9f3ca
박태석 <qkrxotjr9508@gmail.com>
2024-01-01 20:14:57 +0900

    fix : adsense size

--- src/components/modal/adsense/AdsenseModal.style.ts
+++ src/components/modal/adsense/AdsenseModal.style.ts
@@ -6,10 +6,13 @@ export const AdsenseModalWrapper = styled.div`
   height: 100vh;
   z-index: 1000;
   background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const AdContent = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 300px;
+  height: 250px;
   z-index: 99;
 `;
